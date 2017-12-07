95d4b6f1dd0c9708e8124234169f2526620220bc
Romain <rcharpentier10@gmail.com>
2017-12-07 10:07:41 +0100

    Conteneur test
    Fichier de tests pour les conteneurs

---
+++ src/test/ConteneurTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import prog.Conteneur;
+
+public class ConteneurTest {
+
+	@Test
+	public final void editerCellule(){
+		Conteneur c = new Conteneur();
+		try {
+			c.editerCellule("cel", "val");
+			c.editerCellule("cel", "newval");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue("cellule presente dans conteneur",c.getCellule("cel")!=null);
+		assertTrue("valeur de cellule modifiée",c.getCellule("cel").getFormule().compareTo("newval")==0);
+	}
+	
+	@Test
+	public final void removeCellule(){
+		Conteneur c = new Conteneur();
+		try {
+			c.editerCellule("cel", "val");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue("cellule presente dans conteneur",c.getCellule("cel")!=null);
+		c.removeCellule("cel");
+		assertTrue("cellule supprimée du conteneur",c.getAllCellules().isEmpty());
+	}
+}
