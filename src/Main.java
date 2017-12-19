864e037d9de778cf950a7b5ba3d0e31b3f2aed42
Romain <rcharpentier10@gmail.com>
2017-12-19 09:55:34 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,48 @@
 import java.util.Scanner;
 
 import prog.Cellule;
-import prog.Conteneur;
 import prog.Kernel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String nom="", formule="";
+		String nom="", formule="", chemin="", action="";
+		boolean boucle = true;
 		System.out.println("Stop pour quitter");
 		try {
-			while(true) {
-				System.out.println("Nom cellule : ");
-				nom = sc.nextLine();
-				System.out.println("Formule cellule : ");
-				formule = sc.nextLine();
-				if (nom.equalsIgnoreCase("stop") || formule.equalsIgnoreCase("stop")) break;
-				Kernel.editer(nom, formule);
-				System.out.println("Contenu du conteneur :");
-				for (Cellule c : Kernel.getConteneur().getAllCellules()) {
-					System.out.println("cellule(" + c.getName() + ") : " + c.getFormule() + " = " + c.getResultat());
+			while(boucle) {
+				System.out.println("Action : editer | sauvegarder | importer");
+				action = sc.nextLine();
+				switch(action){
+				case "editer":
+					System.out.println("Quel nom ?");
+					nom = sc.nextLine();
+					System.out.println("Quelle formule ?");
+					formule = sc.nextLine();
+					Kernel.editer(nom, formule);
+					break;
+				case "sauvegarder":
+					System.out.println("Quel chemin ?");
+					chemin = sc.nextLine();
+					Kernel.sauvegarder(chemin);
+					break;
+				case "importer":
+					System.out.println("Quel chemin ?");
+					chemin = sc.nextLine();
+					Kernel.importerFichier(chemin);
+					break;
+				case "stop":
+					boucle = false;
+					break;
+				default: System.out.println("Action invalide");
 				}
+				System.out.println("==============================================");
+				System.out.println("===============Contenu du conteneur===========");
+				for (Cellule c : Kernel.getConteneur().getAllCellules()){
+					System.out.println("@" + c.getName() + "(" + c.getFormule() + ") = " + c.getResultat());
+				}
+				System.out.println("==============================================");
 			}
 			sc.close();
 		} catch (Exception e) {
